b11513b3371f475ec9afb2a4afada0df921a9559
pooja-sharma0505 <your-github-email>
2026-06-27 17:02:44 +0530

    Update router

--- router/index.js
+++ router/index.js
@@ -4,13 +4,14 @@ import ProductDetailPage from '../pages/ProductDetailPage.vue'
 import CartPage from '../pages/CartPage.vue'
 import WishlistPage from '../pages/WishlistPage.vue'
 import LoginPage from '../pages/LoginPage.vue'
-
+i
 const routes = [
   { path: '/', name: 'home', component: HomePage },
   { path: '/product/:id', name: 'product-detail', component: ProductDetailPage },
   { path: '/cart', name: 'cart', component: CartPage },
   { path: '/wishlist', name: 'wishlist', component: WishlistPage },
   { path: '/login', name: 'login', component: LoginPage }
+   
 ]
 
 export default createRouter({
